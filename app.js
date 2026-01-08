82bdbbc8e65d591d13c87c76adf08b57c94ea998
LorenzoMinotti-090 <minottilorenzo@gmail.com>
2026-01-08 16:49:41 +0100

    feat: middleware

--- app.js
+++ app.js
@@ -17,6 +17,23 @@ app.use(express.json());
 // registro il router con prefisso /posts
 app.use("/posts", postsRouter);
 
+// middleware 404
+
+app.use((req, res) => {
+  res.status(404).json({
+    error: "Endpoint non trovato"
+  });
+});
+
+// middleware per gestione errori
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+
+  res.status(500).json({
+    error: "Errore interno del server"
+  });
+});
+
 app.listen(port, () => {
   console.log("Server pronto sulla porta " + port);
 });
